0d28b4dfec1772c2fb0a04acb669ee07454603ee
gaurikhasale <97842744+gaurikhasale@users.noreply.github.com>
2022-03-01 19:21:11 +0530

    Assisted

---
+++ Phase3/AssistedPractiseProject/SpringBootHTTPs/src/main/java/com/ecommerce/SpringBootHTTPsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHTTPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHTTPsApplication.class, args);
+	}
+
+}
---
+++ Phase3/AssistedPractiseProject/SpringBootHTTPs/src/main/java/com/ecommerce/controllers/MainController.java
@@ -0,0 +1,28 @@
+package com.ecommerce.controllers;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class MainController {
+
+         @Autowired
+         private Repository repository;
+         
+         @RequestMapping("/")
+          @ResponseBody
+          public String index() {
+               
+
+            return "This is runnning under SSL";
+          }
+         
+        
+}
