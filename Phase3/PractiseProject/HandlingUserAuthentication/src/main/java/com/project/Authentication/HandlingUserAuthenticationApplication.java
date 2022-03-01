beeed5979f64cb66a8f4b7e1cd328771475cb304
gaurikhasale <97842744+gaurikhasale@users.noreply.github.com>
2022-03-01 19:30:40 +0530

    Practise

---
+++ Phase3/PractiseProject/HandlingUserAuthentication/src/main/java/com/project/Authentication/HandlingUserAuthenticationApplication.java
@@ -0,0 +1,24 @@
+package com.project.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.project.Authentication.controllers.AuthenticationController;
+import com.project.Authentication.entities.User;
+import com.project.Authentication.exceptions.UserNotFoundException;
+import com.project.Authentication.services.AuthenticationService;
+
+@SpringBootApplication
+@Import({
+      AuthenticationController.class,
+      UserNotFoundException.class,
+      AuthenticationService.class,
+      User.class
+})
+public class HandlingUserAuthenticationApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(HandlingUserAuthenticationApplication.class, args);
+   }
+
+}
---
+++ Phase3/PractiseProject/HandlingUserAuthentication/src/test/java/com/project/Authentication/HandlingUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingUserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
